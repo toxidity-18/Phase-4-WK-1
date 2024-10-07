60f1c2e39c6b0bb64f22e3bbd4091011137d3171
toxidity-18 <desavigatunganga@gmail.com>
2024-10-07 19:12:00 +0300

    Complete

--- server/app.py
+++ server/app.py
@@ -23,6 +23,125 @@ db.init_app(app)
 def index():
     return '<h1>Code challenge</h1>'
 
+@app.route('/heroes')
+def get_heroes():
+    heros = []
+    for hero in Hero.query.all():
+        hero_dict = hero.to_dict()
+        del hero_dict["hero_powers"]
+        heros.append(hero_dict)
+
+    response = make_response(
+        heros,
+        200,
+        {"Content-Type": "application/json"}
+    )
+
+    return response
+
+@app.route('/heroes/<int:id>')
+def get_hero_by_id(id):
+    hero = Hero.query.filter(Hero.id == id).first()
+    if hero:
+        hero_dict = hero.to_dict()
+        return make_response(hero_dict, 200)
+    else:
+        return make_response({"error": "Hero not found"}, 404)  
+
+
+@app.route('/powers')
+def get_powers():
+    powers = []
+    for power in Power.query.all():
+        power_dict = power.to_dict()
+        del power_dict["hero_powers"]
+        powers.append(power_dict)
+
+    response = make_response(
+        powers,
+        200,
+        {"Content-Type": "application/json"}
+    )
+
+    return response
+
+@app.route('/powers/<int:id>')
+def get_power_by_id(id):
+    power = Power.query.filter(Power.id == id).first()
+    if power:
+        power_dict = power.to_dict()
+        del power_dict['hero_powers'] 
+        return make_response(power_dict, 200)
+    else:
+        return make_response({"error": "Power not found"}, 404)
+
+
+@app.route('/powers/<int:id>', methods=['PATCH'])
+def update_power(id):
+    power = Power.query.filter(Power.id == id).first()
+
+    if not power:
+        return make_response({"error": "Power not found"}, 404)
+
+    data = request.get_json()
+    description = data.get("description")
+
+  
+    if not description or len(description) < 20:
+        return make_response({"errors": ["validation errors"]}, 400)
+
+   
+    power.description = description
+    db.session.commit()
+
+    return make_response({
+        "id": power.id,
+        "name": power.name,
+        "description": power.description
+    }, 200)
+
+
+@app.route('/hero_powers', methods=['POST'])
+def create_hero_power():
+    data = request.get_json()
+
+    strength = data.get("strength")
+    power_id = data.get("power_id")
+    hero_id = data.get("hero_id")
+
+   
+    if strength not in ["Strong", "Weak", "Average"]:
+        return make_response({"errors": ["validation errors"]}, 400)
+
+    hero = Hero.query.filter(Hero.id == hero_id).first()
+    power = Power.query.filter(Power.id == power_id).first()
+
+    if not hero or not power:
+        return make_response({"errors": ["Hero or Power not found"]}, 404)
+
+    
+    hero_power = HeroPower(strength=strength, hero_id=hero_id, power_id=power_id)
+    db.session.add(hero_power)
+    db.session.commit()
+
+    return make_response({
+        "id": hero_power.id,
+        "hero_id": hero.id,
+        "power_id": power.id,
+        "strength": hero_power.strength,
+        "hero": {
+            "id": hero.id,
+            "name": hero.name,
+            "super_name": hero.super_name
+        },
+        "power": {
+            "id": power.id,
+            "name": power.name,
+            "description": power.description
+        }
+    }, 200)
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
+
+# imports
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
@@ -19,8 +21,12 @@ class Hero(db.Model, SerializerMixin):
     super_name = db.Column(db.String)
 
     # add relationship
+    hero_powers = db.relationship(
+        'HeroPower',back_populates='hero', cascade="all, delete-orphan",
+    )
 
     # add serialization rules
+    serialize_rules = ('-hero_powers.hero',)
 
     def __repr__(self):
         return f'<Hero {self.id}>'
@@ -34,10 +40,21 @@ class Power(db.Model, SerializerMixin):
     description = db.Column(db.String)
 
     # add relationship
+    hero_powers = db.relationship(
+        'HeroPower', back_populates='power', cascade="all, delete-orphan",
+    )
 
     # add serialization rules
+    serialize_rules = ('-hero_powers.power',)
 
     # add validation
+
+    @validates('description')
+    def validates_description(self, key, description):
+        if len(description) < 20:
+            raise ValueError('validation errors')
+        return description
+    
 
     def __repr__(self):
         return f'<Power {self.id}>'
@@ -49,11 +66,23 @@ class HeroPower(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     strength = db.Column(db.String, nullable=False)
 
+    # add foreign keys
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+
     # add relationships
+    hero = db.relationship('Hero', back_populates='hero_powers')
+    power = db.relationship('Power', back_populates='hero_powers')
 
     # add serialization rules
-
+    serialize_rules = ('-hero', '-power')
     # add validation
-
+    @validates('strength')
+    def validate_strength(self, key, strength):
+        valid_strengths = ['Strong', 'Weak', 'Average']
+        if strength not in valid_strengths:
+            raise ValueError(f"Strength must be one of {valid_strengths}.")
+        return strength
+    
     def __repr__(self):
         return f'<HeroPower {self.id}>'
